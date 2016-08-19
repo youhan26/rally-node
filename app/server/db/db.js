0db7ce55c2a6b9a8753df7b471d62b962c54be28
YouHan26 <youhan26@gmail.com>
2016-08-19 20:39:19 +0800

    update sql

---
+++ app/server/db/db.js
@@ -0,0 +1,20 @@
+/**
+ * Created by YouHan on 2016/8/19.
+ */
+var Dao = require('mysql-orm-builder').dao;
+var config = {
+  host: '52.196.207.191',
+  port: 3306,
+  database: 'rally',
+  user: 'youto',
+  password: '123456',
+  connectionLimit: 10,
+  dateStrings: true,
+  supportBigNumbers: true, //enable big number
+  bigNumberStrings: true
+};
+
+
+var dao = new Dao(config);
+
+module.exports = dao;
