481ec25b21162deffc9136cd2f65c7cb3792054a
Han You <youhan26@gmail.com>
2016-10-24 18:18:57 +0800

    update

--- app/server/db/builder.js
+++ app/server/db/builder.js
@@ -5,11 +5,11 @@ var Builder = require('mysql-orm-builder').orm;
 
 
 var builder = new Builder({
-    host: '45.32.53.48',
+    host: 'IP',
     port: 3306,
     database: 'rally',
-    user: 'youhan',
-    password: 'youhan',
+    user: 'user',
+    password: 'password',
     connectionLimit: 10,
     dateStrings: true,
     supportBigNumbers: true, //enable big number
