1c10fd71a3cfe062cd81ce6960921bd6f6cba44a
Han You <youhan26@gmail.com>
2016-10-24 18:18:57 +0800

    update

--- app/server/db/builder.js
+++ app/server/db/builder.js
@@ -5,11 +5,11 @@
 
 
 ***REMOVED***
-    host: '45.32.53.48',
 ***REMOVED***
 ***REMOVED***
-    user: 'youhan',
-    password: 'youhan',
+***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
