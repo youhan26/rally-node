4643e7275ce71d4f626a78792f328f7c71c4c4bf
YouHan26 <youhan26@gmail.com>
2016-11-17 00:16:05 +0800

    update style

--- app/client/components/mixins/mixins-api.jsx
+++ app/client/components/mixins/mixins-api.jsx
@@ -12,49 +12,48 @@ const ALL = 'all';
  * split single table request to one component
  */
 export default {
-    _registry(url){
-        this._mixins = {
-            url: url
-        }
-    },
-    _getById(id){
-        const me = this;
-        return api.get({
-            url: prefix + me._mixins.url + prefix + id
-        });
-    },
-    _getByParams(params){
-        const me = this;
-        return api.get({
-            url: prefix + me._mixins.url,
-            params: params
-        });
-    },
-    _getList(params){
-        const me = this;
-        return api.get({
-            url: prefix + me._mixins.url + prefix + ALL,
-            params: params
-        });
-    },
-    _save(data, id){
-        const me = this;
-        if (id) {
-            return api.patch({
-                url: prefix + me._mixins.url + prefix + id,
-                data: data
-            })
-        } else {
-            return api.post({
-                url: prefix + me._mixins.url,
-                data: data
-            });
-        }
-    },
-    _removeById(id){
-        const me = this;
-        return api.del({
-            url: prefix + me._mixins.url + prefix + id
-        });
+  _registry(url2){
+    this.mixinsData = {
+      url: url2
+    };
+  },
+  _getById(id){
+    const me = this;
+    return api.get({
+      url: prefix + me.mixinsData.url + prefix + id
+    });
+  },
+  _getByParams(params2){
+    const me = this;
+    return api.get({
+      url: prefix + me.mixinsData.url,
+      params: params2
+    });
+  },
+  _getList(params2){
+    const me = this;
+    return api.get({
+      url: prefix + me.mixinsData.url + prefix + ALL,
+      params: params2
+    });
+  },
+  _save(data2, id){
+    const me = this;
+    if (id) {
+      return api.patch({
+        url: prefix + me.mixinsData.url + prefix + id,
+        data: data2
+      });
     }
-}
+    return api.post({
+      url: prefix + me.mixinsData.url,
+      data: data2
+    });
+  },
+  _removeById(id){
+    const me = this;
+    return api.del({
+      url: prefix + me.mixinsData.url + prefix + id
+    });
+  }
+};
