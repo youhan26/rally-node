ab60b4ce93ddaae88008be6bb2bd34c328dc728e
YouHan26 <youhan26@gmail.com>
2016-11-19 15:49:40 +0800

    update style

--- app/client/components/story/search/storySearch.jsx
+++ app/client/components/story/search/storySearch.jsx
@@ -62,14 +62,14 @@ export default class StorySearch extends Component {
                 />
               </FormItem>
               <FormItem
-                label="Release"
+                label="Project"
                 labelCol={{span: 10}}
                 wrapperCol={{span: 14}}
               >
-                <ReleaseSelect
-                  projectId={this.props.condition.projectId}
-                  value={this.props.condition.releaseId}
-                  onChange={this.releaseChange}
+                <CommonSelect
+                  className="full-width" url="/project/all"
+                  value={this.props.condition.projectId}
+                  onChange={this.projectChange}
                 />
               </FormItem>
             </Col>
@@ -85,14 +85,14 @@ export default class StorySearch extends Component {
                 />
               </FormItem>
               <FormItem
-                label="Project"
+                label="Release"
                 labelCol={{span: 10}}
                 wrapperCol={{span: 14}}
               >
-                <CommonSelect
-                  className="full-width" url="/project/all"
-                  value={this.props.condition.projectId}
-                  onChange={this.projectChange}
+                <ReleaseSelect
+                  projectId={this.props.condition.projectId}
+                  value={this.props.condition.releaseId}
+                  onChange={this.releaseChange}
                 />
               </FormItem>
             </Col>
